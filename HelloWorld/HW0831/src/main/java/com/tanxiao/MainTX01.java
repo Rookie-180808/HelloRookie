c425af5c85681c9f2ed0fad04bdcdfab60aa55b4
tanxiao1992 <18810818703@163.com>
2018-09-03 19:01:07 +0800

    8.24-第一题-100%

--- HelloWorld/HW0831/src/main/java/com/tanxiao/MainTX01.java
+++ HelloWorld/HW0831/src/main/java/com/tanxiao/MainTX01.java
@@ -1,9 +1,25 @@
 package com.tanxiao;
 
+import java.util.Scanner;
+
 /**
  * @author TanXiao
  * Created on 2018/08/31 20:20
  */
 
 public class MainTX01 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int k = scanner.nextInt();
+        int a=scanner.nextInt();
+        int x=scanner.nextInt();
+        int b=scanner.nextInt();
+        int y=scanner.nextInt();
+        int t=type(k,a,x,b,y);
+        System.out.println(t);
+    }
+
+    private static int type(int k, int a, int x, int b, int y) {
+        return 9;
+    }
 }
