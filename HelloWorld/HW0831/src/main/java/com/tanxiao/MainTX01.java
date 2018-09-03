63fc740e452472deda4dd45f3232259636ea9fe8
Pxshuo <Pxshuo2415980327@163.com>
2018-09-03 18:53:22 +0800

    0903-Keep-笔试

--- HelloWorld/HW0831/src/main/java/com/tanxiao/MainTX01.java
+++ HelloWorld/HW0831/src/main/java/com/tanxiao/MainTX01.java
@@ -1,9 +1,14 @@
 package com.tanxiao;
 
+import java.util.Scanner;
+
 /**
  * @author TanXiao
  * Created on 2018/08/31 20:20
  */
 
 public class MainTX01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+    }
 }
