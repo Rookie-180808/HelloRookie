ce83a6431b20dbd0bb85d29cb3a5e7d5e25abda1
Pxshuo <Pxshuo2415980327@163.com>
2018-09-09 16:10:58 +0800

    0909-JD-init

---
+++ HelloWorld/HW0909/src/main/java/com/pxshuo/MainJd01.java
@@ -0,0 +1,16 @@
+package com.pxshuo;
+
+import java.util.Scanner;
+
+/**
+ * @author Pxshuo
+ * Created on 2018/09/09 16:10
+ */
+
+public class MainJd01 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        in.close();
+    }
+}
